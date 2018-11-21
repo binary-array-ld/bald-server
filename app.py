7ce28b1b07403f3d451eb5b747e5b82943719158
Jonathan Yu <jonathan.yu@csiro.au>
2018-11-21 17:02:18 +1100

    adding auth for POST and css

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
-from flask import Flask, redirect, url_for, request, render_template, jsonify
+from functools import wraps
+from flask import Flask, redirect, url_for, request, render_template, jsonify, abort
 from pymongo import MongoClient
 from bson import json_util
 from bson.json_util import dumps
-
 import json
-
+import uuid
+from flask.logging import default_handler
 
 app = Flask(__name__)
 
@@ -13,6 +14,43 @@ client = MongoClient(
     os.environ['DB_PORT_27017_TCP_ADDR'],
     27017)
 db = client.datasets
+
+uuid.uuid4().hex
+
+if 'SECRET_KEY' in os.environ and os.environ['SECRET_KEY'] is not None:
+   authtoken = os.environ['SECRET_KEY']
+#   app.logger.debug("Auth token is " + str(authtoken))
+else:
+   #generate a unique auth token
+   authtoken = uuid.uuid4().hex
+#   app.logger.debug("Auth token is " + str(authtoken))
+   app.logger.debug("Use this for posting content")
+authtoken = "SECRET"
+print("Auth token is " + str(authtoken))
+
+def authorize(f):
+    @wraps(f)
+    def decorated_function(*args, **kws):
+            print(request)
+            if not 'Authorization' in request.headers:
+               abort(401)
+
+            user = None
+            data = request.headers['Authorization'].encode('ascii','ignore')
+            token = str.replace(str(data), 'Bearer ','')
+            #try:
+            #    user = jwt.decode(token, JWT_SECRET, algorithms=['HS256'])['sub']
+            #except:
+            #    abort(401)
+            if token != authtoken:
+               abort(401)
+            return f(user, *args, **kws)            
+    return decorated_function
+
+def isAuthorised(token):
+   if token == authtoken:
+      return True
+   return False
 
 @app.route('/')
 def list():
@@ -37,11 +75,18 @@ def view():
 @app.route('/new', methods=['POST'])
 def new():
     app.logger.debug("JSON received...")
-    app.logger.debug(request.json)
+    if not 'Authorization' in request.headers:
+       abort(401)
+    print(request.headers['Authorization'])
+    data = request.headers['Authorization']
+    token = str.replace(str(data), 'Bearer ', '')
+    if token != authtoken: 
+       return jsonify({ 'result' : 'not authorised' }),401
     content = request.json
-    db.datasets.insert_one(content)
+    #db.datasets.insert_one(content, check_keys=False)
+    db.datasets.insert(content, check_keys=False)
 
-    return jsonify({ 'result' : 'success' })
+    return jsonify({ 'result' : 'success' }),200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
--- example-uploader.sh
+++ example-uploader.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 DIR=$1
+AUTHTOKEN=$2
+declare -a curlArgs=('-H' "Content-Type: application/json" '-H' "Authorization: Bearer ${AUTHTOKEN}")
+
 for f in $(find $1 -name '*.json'); 
 do 
-   curl -vX POST http://localhost:4000/new -d @${f} --header "Content-Type: application/json"
+   echo "curl -vX POST http://localhost:4000/new?token=${AUTHTOKEN} -d @${f} ${curlArgs[@]}\""
+   curl -vX POST http://localhost:4000/new -d @${f} "${curlArgs[@]}"
 done
